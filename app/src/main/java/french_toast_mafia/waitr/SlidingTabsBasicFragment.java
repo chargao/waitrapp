dc1661de1476d79193186da03ba2daa22f82ac44
MrLanks <travispatrick@utexas.edu>
2015-05-06 06:36:17 -0500

    working on frontend recieving and sending to backend

--- app/src/main/java/french_toast_mafia/waitr/SlidingTabsBasicFragment.java
+++ app/src/main/java/french_toast_mafia/waitr/SlidingTabsBasicFragment.java
@@ -18,10 +18,14 @@ package french_toast_mafia.waitr;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,6 +37,8 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -42,6 +48,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import french_toast_mafia.view.SlidingTabLayout;
@@ -60,7 +67,6 @@ public class SlidingTabsBasicFragment extends Fragment {
     Context context;
     String regid;
 
-
     private SlidingTabLayout mSlidingTabLayout;
     private ViewPager mViewPager;
     private static final int PAGE_COUNT = 2; // change this to change number of pages in app
@@ -71,20 +77,22 @@ public class SlidingTabsBasicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.fragment_sample, container, false);
 
+        context = view.getContext();
         // Check device for Play Services APK. If check succeeds, proceed with
         //  GCM registration.
-//        if (checkPlayServices()) {
-//            gcm = GoogleCloudMessaging.getInstance(this);
-//            regid = getRegistrationId(context);
-//
-//            if (regid.isEmpty()) {
-//                registerInBackground();
-//            }
-//        } else {
-//            Log.i(TAG, "No valid Google Play Services APK found.");
-//        }
-        return inflater.inflate(R.layout.fragment_sample, container, false);
+        if (checkPlayServices()) {
+            gcm = GoogleCloudMessaging.getInstance(context);
+            regid = getRegistrationId(context);
+
+            if (regid.isEmpty()) {
+                registerInBackground();
+            }
+        } else {
+            Log.i(TAG, "No valid Google Play Services APK found.");
+        }
+        return view;
     }
 
     /**
@@ -233,8 +241,33 @@ public class SlidingTabsBasicFragment extends Fragment {
                 final Button button = (Button) view.findViewById(R.id.btn_check_in);
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(View v) {
+                    public void onClick(final View v) {
                         Toast.makeText(v.getContext(), "Checked In", Toast.LENGTH_LONG).show();
+                        AsyncTask execute = new AsyncTask() {
+                            @Override
+                            protected Object doInBackground(Object[] params) {
+                                String msg = "";
+                                try {
+                                    Bundle data = new Bundle();
+                                    data.putString("my_message", "Hello World");
+                                    data.putString("my_action",
+                                            "com.google.android.gcm.demo.app.ECHO_NOW");
+                                    String id = Integer.toString(msgId.incrementAndGet());
+                                    //gcm.send(SENDER_ID + "@gcm.googleapis.com", id, data);
+                                    gcm.send(SENDER_ID + "@gcm.googleapis.com", id, data);
+                                    msg = "Sent message";
+                                } catch (IOException ex) {
+                                    msg = "Error :" + ex.getMessage();
+                                }
+                                return msg;
+                            }
+
+
+                            //@Override
+                            protected void onPostExecute(String msg) {
+
+                            }
+                        }.execute(null, null, null);
                     }
                 });
 
@@ -283,38 +316,6 @@ public class SlidingTabsBasicFragment extends Fragment {
                     }
                 });
             }
-               /* listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view,
-                                            int position, long id) {
-
-                        // ListView Clicked item index
-                        int itemPosition = position;
-
-                        // ListView Clicked item value
-                        String itemValue = (String) listView.getItemAtPosition(position);
-
-                        // Show Alert
-                        Toast.makeText(view.getContext(),
-                                "You've got a coupon for " + itemValue, Toast.LENGTH_LONG)
-                                .show();
-
-                    }
-                });
-            }*/
-
-
-       /*
-         // Retrieve a ListView from the inflated View, and update it's list
-        listView = (ListView) view.findViewById(R.id.list);
-        // Defined Array values to show in ListView
-
-        //listView.setAdapter(rm);
-        // Retrieve a TextView from the inflated View, and update it's text
-        title = (TextView) view.findViewById(R.id.item_title);
-        title.setText(x);
-
-      }*/
             if (position > (PAGE_COUNT - 1)) { //not a page
                 view = getActivity().getLayoutInflater().inflate(R.layout.pager_item,
                         container, false);
@@ -362,5 +363,151 @@ public class SlidingTabsBasicFragment extends Fragment {
 
     }
 
+    /**
+     * Gets the current registration ID for application on GCM service.
+     * <p/>
+     * If result is empty, the app needs to register.
+     *
+     * @return registration ID, or empty string if there is no existing
+     * registration ID.
+     */
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing registration ID is not guaranteed to work with
+        // the new app version.
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+    ///...
+
+    /**
+     * @return Application's {@code SharedPreferences}.
+     */
+    private SharedPreferences getGCMPreferences(Context context) {
+        /*
+        This sample app persists the registration ID in shared preferences, but
+        how you store the registration ID in your app is up to you.
+        */
+        return context.getSharedPreferences(MainActivity.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * <p/>
+     * Stores the registration ID and app versionCode in the application's
+     * shared preferences.
+     */
+    private void registerInBackground() {
+        new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+
+                    // You should send the registration ID to your server over HTTP,
+                    // so it can use GCM/HTTP or CCS to send messages to your app.
+                    // The request to your server should be authenticated if your app
+                    // is using accounts.
+                    sendRegistrationIdToBackend();
+
+                    // For this demo: we don't need to send it because the device
+                    // will send upstream messages to a server that echo back the
+                    // message using the 'from' address in the message.
+
+                    // Persist the registration ID - no need to register again.
+                    storeRegistrationId(context, regid);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+
+            protected void onPostExecute(String msg) {
+                Log.i(TAG, msg + "\n");
+            }
+        }.execute(null, null, null);
+        //...
+    }
+
+    /**
+     * Sends the registration ID to your server over HTTP, so it can use GCM/HTTP
+     * or CCS to send messages to your app. Not needed for this demo since the
+     * device sends upstream messages to a server that echoes back the message
+     * using the 'from' address in the message.
+     */
+    private void sendRegistrationIdToBackend() {
+        // Your implementation here.
+    }
+
+    /**
+     * Stores the registration ID and app versionCode in the application's
+     * {@code SharedPreferences}.
+     *
+     * @param context application's context.
+     * @param regId   registration ID
+     */
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this.getActivity());
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this.getActivity(),
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                //finish();
+            }
+            return false;
+        }
+        return true;
+    }
 
 }
